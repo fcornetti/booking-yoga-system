91c374a9340387d4cda5f76b614c4abd52d9263a
Lith <LithWang@outlook.com>
2025-02-16 17:04:19 +0800

    static file

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from operator import and_
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint, func
 from datetime import datetime
 from flask_cors import CORS
+import os.path
 
 app = Flask(__name__)
 CORS(app)
@@ -229,6 +230,37 @@ def get_classes():
     } for yoga_class, booking_count in classes_with_bookings_count]
 
     return jsonify(result)
+
+def root_dir():  # pragma: no cover
+    return os.path.abspath(os.path.dirname(__file__))
+
+
+def get_file(filename):  # pragma: no cover
+    try:
+        src = os.path.join(root_dir(), filename)
+        # Figure out how flask returns static files
+        # Tried:
+        # - render_template
+        # - send_file
+        # This should not be so non-obvious
+        return open(src).read()
+    except IOError as exc:
+        return str(exc)
+
+@app.route('/', defaults={'path': 'index.html'})
+@app.route('/<path:path>')
+def get_resource(path):  # pragma: no cover
+    mimetypes = {
+        ".css": "text/css",
+        ".html": "text/html",
+        ".js": "application/javascript",
+    }
+    complete_path = os.path.join(root_dir(),"static", path)
+    ext = os.path.splitext(path)[1]
+    mimetype = mimetypes.get(ext, "text/html")
+    content = get_file(complete_path)
+    return Response(content, mimetype=mimetype)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
